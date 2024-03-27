e26bbeca262c470b08760e77b4d265fd94118b98
satendra1729 <satendra1729@gmail.com>
2024-03-27 22:42:44 +0530

    cpp practice

--- cpp/demo.cpp
+++ cpp/demo.cpp
@@ -7,4 +7,10 @@ int main () {
     int* ptr = &a; 
     int** ptrofptr = &ptr;
     std::cout << add(1,2) << std::endl;
+
+    int favorite_number;
+    std::cout << "Please enter your favorite number between 1 and 100" << std::endl;
+    std::cin >> favorite_number;
+    std::cout << "This is my favorite number too" << std::endl;
+    std::cout << "Really " << favorite_number << " is my favorite number" << std::endl;
 }
